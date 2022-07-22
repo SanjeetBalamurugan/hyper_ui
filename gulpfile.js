607df639f361a5762a74a0c2ff1a2b6608c26ed8
TT <xwTT@users.noreply.github.com>
2022-07-22 19:39:14 +0530

    add topography, changed output name,  and added ts instead of direct js
    it is a min css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,31 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+var rename = require('gulp-rename');
+var csso = require('gulp-csso');
+var autoprefixer = require('gulp-autoprefixer');
+
+const AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
 
 function buildStyles() {
   return src('hyperui/**/*.scss')
-    .pipe(sass())
+    .pipe(sass({
+      precision: 10,
+      includePaths: ['.'],
+      onError: console.error.bind(console, 'Sass error:')
+    }))
+    .pipe(autoprefixer({ browsers: AUTOPREFIXER_BROWSERS }))
+    .pipe(csso())
+    .pipe(rename('hui.css'))
     .pipe(dest('css'))
 }
 
---
+++ ts/hui.ts
@@ -0,0 +1,23 @@
+// this is hyper ui's javascript file pls do not edit anything here
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const HUIalert = (message:any, type:any) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder!.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    HUIalert('Nice, you triggered this alert message!', 'success')
+    console.log("Alert Trigger Activated")
+  })
+}
